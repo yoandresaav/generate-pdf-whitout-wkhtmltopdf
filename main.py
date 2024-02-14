18d4f7c1ae01c6db55a890dd928a041c16e5e877
Yoandre Saavedra Gonzalez <2855358+yoandresaav@users.noreply.github.com>
2024-02-14 13:19:04 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from xhtml2pdf import pisa             # import python module
+import os
+
+# Define your data
+output_filename = "test.pdf"
+
+# Utility function
+def convert_html_to_pdf(source_html, output_filename):
+    # open output file for writing (truncated binary)
+    result_file = open(output_filename, "w+b")
+
+    # convert HTML to PDF
+    pisa_status = pisa.CreatePDF(
+            source_html,                # the HTML to convert
+            dest=result_file)           # file handle to recieve result
+
+    # close output file
+    result_file.close()                 # close output file
+
+    # return False on success and True on errors
+    return pisa_status.err
+
+# Main program
+if __name__ == "__main__":
+    pisa.showLogging()
+    with open('hello.html', 'r') as file:
+        filedata = file.read()
+        convert_html_to_pdf(filedata, output_filename)
