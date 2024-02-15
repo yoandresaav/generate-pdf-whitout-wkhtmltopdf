a0f1d2f13687ad38d5e8611558bc480e30eff212
Yoandre Saavedra Gonzalez <2855358+yoandresaav@users.noreply.github.com>
2024-02-15 13:12:11 -0600

    fix document flow

--- generator.py
+++ generator.py
@@ -1,8 +1,12 @@
 from xhtml2pdf import pisa             # import python module
 import os
+import pprint as pp
 
 # Define your data
-output_filename = "test.pdf"
+template_report = "pdf_template.html"
+output_filename = "report.pdf"
+analysis_text = "analysis.txt"
+
 
 # Utility function
 def convert_html_to_pdf(source_html, output_filename):
@@ -20,9 +24,26 @@ def convert_html_to_pdf(source_html, output_filename):
     # return False on success and True on errors
     return pisa_status.err
 
+def get_text_from_file(filename):
+    with open(filename, 'r') as file:
+        return file.read()
+
 # Main program
 if __name__ == "__main__":
     pisa.showLogging()
-    with open('hello.html', 'r') as file:
+
+    # load text template
+    template_text =  get_text_from_file(analysis_text)
+
+    # divide the text into paragraphs
+    paragraphs = template_text.split("\n\n")
+
+    # add paragraphs to the template
+    paragraphs_html = ['<p class="paragraph">{}</p>'.format(paragraph) for paragraph in paragraphs]
+
+
+    with open(template_report, 'r') as file:
         filedata = file.read()
+        filedata = filedata.replace("{{analysis}}", "\n".join(paragraphs_html))
+        print(filedata) 
         convert_html_to_pdf(filedata, output_filename)
